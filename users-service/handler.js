b366280305e4cb3cd57f7054d07865076a2ea4c2
Raman Plaha <ramanplaha1990@gmail.com>
2020-07-06 10:58:16 +0100

    Increase password length and username length

--- users-service/handler.js
+++ users-service/handler.js
@@ -72,26 +72,14 @@ function signToken(id) {
 }
 
 function checkIfInputIsValid(eventBody) {
-  if (!(eventBody.password && eventBody.password.length >= 7)) {
+  if (!(eventBody.password && eventBody.password.length >= 6)) {
     return Promise.reject(
       new Error(
-        'Password error. Password needs to be longer than 8 characters.'
+        'Password error. Password needs to be longer than 7 characters.'
       )
     );
   }
-
-  if (
-    !(
-      eventBody.name &&
-      eventBody.name.length > 5 &&
-      typeof eventBody.name === 'string'
-    )
-  )
-    return Promise.reject(
-      new Error('Username error. Username needs to longer than 5 characters')
-    );
-
-  if (!(eventBody.email && typeof eventBody.name === 'string'))
+  if (!eventBody.email)
     return Promise.reject(
       new Error('Email error. Email must have valid characters.')
     );
@@ -113,7 +101,6 @@ function signUp(eventBody) {
     .then(
       (hash) =>
         User.create({
-          name: eventBody.name,
           email: eventBody.email,
           password: hash,
         })
